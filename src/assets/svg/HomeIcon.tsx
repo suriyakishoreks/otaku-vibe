201de7711d88a7e1fcaa004dad3725953316acf6
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-16 15:30:28 +0530

    feat: add icons

---
+++ src/assets/svg/HomeIcon.tsx
@@ -0,0 +1,9 @@
+import type { IconProps } from './Util'
+
+function HomeIcon({ size, color }: IconProps) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" height={size} viewBox="0 0 24 24" width={size} fill={color}><path d="M0 0h24v24H0z" fill="none" /><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" /></svg>
+    )
+}
+
+export default HomeIcon
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Logo } from "../Logo"
+import Logo from "../../assets/svg/LogoIcon";
 
 interface HeaderProps {
 
