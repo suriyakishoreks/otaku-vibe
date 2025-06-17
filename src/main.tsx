ba20f6498b62ab8bad630748301ab4b5aea36f9e
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-17 17:10:47 +0530

    feat: store init

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.scss'
 import App from './App.tsx'
+import { store } from './store'
+import { Provider as ReduxProvider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
   </StrictMode>,
 )
