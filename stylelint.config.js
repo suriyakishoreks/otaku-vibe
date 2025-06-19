9a55f3df39b2f78384559c72da77035a11c44a9a
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-19 13:53:05 +0530

    fix: add bem css pattern check

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,6 +13,12 @@ export default {
     "stylelint-order", // To enforce property order
   ],
   rules: {
+    "selector-class-pattern": [
+      "^[a-z]([a-z0-9-]+)?(__([a-z0-9]+(-[a-z0-9]+)*))?(--([a-z0-9]+(-[a-z0-9]+)*))?$",
+      {
+        message: "Expected BEM class name (block__element--modifier)",
+      },
+    ],
     "property-disallowed-list": [
       "font-family", // Disallow font-family property
       "font-size", // Disallow font-size property
