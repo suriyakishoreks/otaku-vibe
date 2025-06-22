102c544be86114f2d72eff8a9a80d36cf9dfab81
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-22 18:19:18 +0530

    feat: progress in header logic

--- src/components/widgets/header/Header.tsx
+++ src/components/widgets/header/Header.tsx
@@ -8,18 +8,38 @@ import SettingIcon from "../../atoms/icons/SettingIcon";
 import { Pill } from "../../atoms/pill";
 import styles from "./Header.module.scss";
 import GithubIcon from "../../atoms/icons/GithubIcon";
+import { Link } from "react-router";
+
+function HeaderNav() {
+    return <nav className={styles.header__nav}>
+        <Link to='/' >
+            <Pill icon={HomeIcon} text="Home" />
+        </Link>
+        <Link to='/anime'>
+            <Pill icon={AnimeIcon} text='Anime' />
+        </Link>
+        <Link to='/manga'>
+            <Pill icon={MangaIcon} text="Manga" />
+        </Link>
+    </nav>;
+}
 
 function Header() {
     const headerRef = useRef<HTMLHeadElement>(null);
+    const maxHeaderWidth = useRef<number>(0);
     const [isOverflowing, setIsOverflowing] = useState(false);
 
     // TODO: Implement this properly as custom hook
     useEffect(() => {
         const checkOverflow = () => {
             if (headerRef.current) {
-                setIsOverflowing(
-                    headerRef.current.scrollWidth > headerRef.current.clientWidth
-                );
+                if (headerRef.current.scrollWidth > headerRef.current.clientWidth && maxHeaderWidth.current === 0) {
+                    setIsOverflowing(true);
+                    maxHeaderWidth.current = headerRef.current.scrollWidth;
+                } else if (maxHeaderWidth.current > 0 && headerRef.current.scrollWidth > maxHeaderWidth.current) {
+                    setIsOverflowing(false);
+                    maxHeaderWidth.current = 0;
+                }
             }
         };
         checkOverflow();
@@ -31,15 +51,7 @@ function Header() {
         <header className={styles.header} ref={headerRef}>
             <div className={styles.header__lhs} >
                 <Logo />
-                {!isOverflowing && (
-                    <nav className={styles.header__nav}>
-                        <Pill icon={HomeIcon} text="Home" />
-                        <Pill icon={AnimeIcon} text='Anime' />
-                        <Pill icon={MangaIcon} text="Manga" />
-                        <Pill icon={MangaIcon} text="Manhwa" />
-                        <Pill icon={MangaIcon} text="Manhua" />
-                    </nav>
-                )}
+                {!isOverflowing && <HeaderNav />}
             </div>
             <div className={styles.header__rhs}>
                 <GithubIcon size={30} color='s-color-fg-primary' />
