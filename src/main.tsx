c6c545abf29df52d48a0ebf60ef1970bcac38f9a
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-22 18:16:13 +0530

    feat: add font preload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { preload } from 'react-dom';
 import './styles/main.scss';
 import { store } from './store';
 import { Provider as ReduxProvider } from 'react-redux';
 import { RouterProvider } from 'react-router';
 import router from './router';
+
+preload('./font/outfit-variable.woff2', { as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
