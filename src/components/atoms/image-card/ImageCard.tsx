ec13eadf48f6937e99fc8b55c1d73e3e8e5a9f30
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-23 19:40:24 +0530

    feat: image card atom

---
+++ src/components/atoms/image-card/ImageCard.tsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router';
+import { Image } from '../image';
+import { Label } from '../label';
+import styles from './ImageCard.module.scss';
+import classNames from 'classnames';
+
+interface ImageCardProps {
+    src: string;
+    alt: string;
+    navigateTo?: string;
+    title?: string;
+}
+
+function ImageCard({ src, alt, navigateTo, title }: ImageCardProps) {
+    const content = (
+        <div className={classNames({ [styles['image-card']]: true, [styles['image-card__clickable']]: navigateTo, 'no-text-select': true })}>
+            <Image src={src} alt={alt} className={styles['image-card__image']} />
+            {title && <div className={styles['image-card__bottom-overlay']}>
+                <Label as='h4' font='typo-primary-m-medium' className={styles['image-card__title']} >{title}</Label>
+            </div>}
+        </div>
+    );
+
+    return navigateTo ? (
+        <Link to={navigateTo}>{content}</Link>
+    ) : (
+        content
+    );
+}
+
+export default ImageCard;
