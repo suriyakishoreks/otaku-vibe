fa63b8be6134c3d2788668cb383db2fac799494e
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-23 19:38:17 +0530

    fix: proload path

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { RouterProvider } from 'react-router';
 import router from './router';
 
-preload('./font/outfit-variable.woff2', { as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' });
+preload(`${import.meta.env.BASE_URL}font/outfit-variable.woff2`, { as: 'font', type: 'font/woff2', crossOrigin: 'anonymous' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
