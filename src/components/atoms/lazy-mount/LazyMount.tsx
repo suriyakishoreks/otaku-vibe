11d52d7b5a62e864286c7f411aa659e4312d7b5c
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-24 19:18:12 +0530

    feature: lazy mount component

---
+++ src/components/atoms/lazy-mount/LazyMount.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import useIntersectionObserver from '../../../shared/hooks/useIntersectionObserver';
+
+interface LazyMountProps {
+    children: React.ReactNode;
+    rootMargin?: string;
+    threshold?: number;
+    estimatedHeight: number;
+    wrapperClassName?: string;
+}
+
+const LazyMount = ({
+    children,
+    rootMargin = '0px 0px 250px 0px',
+    threshold = 0,
+    estimatedHeight,
+    wrapperClassName,
+}: LazyMountProps) => {
+    const [wrapperRef, isIntersecting] = useIntersectionObserver({
+        root: null,
+        rootMargin,
+        threshold,
+    });
+
+    const [hasMounted, setHasMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        if (isIntersecting && !hasMounted) {
+            setHasMounted(true);
+        }
+    }, [isIntersecting, hasMounted]);
+
+    return (
+        <div ref={wrapperRef} style={!hasMounted ? { height: estimatedHeight } : {}} className={wrapperClassName}>
+            {hasMounted ? children : null}
+        </div>
+    );
+};
+
+export default LazyMount;
