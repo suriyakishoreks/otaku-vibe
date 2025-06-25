2a16e98ef716f05d4bbef9870ee0f388cfb9dd2a
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-25 19:58:21 +0530

    feat: homepage progress

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,56 +1,57 @@
-import { useGetAnimeSeasonsNowQuery, useGetTopAnimeQuery, useGetTopMangaQuery, useGetAnimeSeasonsUpcomingQuery } from "../../services/jikan";
+import { useGetTopAnimeQuery, useGetTopMangaQuery, useGetTopCharactersQuery, useGetTopPeopleQuery } from "../../services/jikan";
 import { LazyMount } from "../../components/atoms/lazy-mount";
 import { HorizontalCarousel } from "../../components/widgets/horizontal-carousel";
+import Vernac from "../../services/vernac";
 
 function HomePage() {
-
     return (
         <div>
+            <HorizontalCarousel
+                heading={Vernac.getVernac('HP_TOP_ANIME_TITLE')}
+                type="centered"
+                useQueryHook={useGetTopAnimeQuery}
+                options={{}}
+                adapter={(data) => data?.data.map((anime) => ({
+                    title: anime.title,
+                    imageUrl: anime.images.jpg.image_url,
+                    navigateTo: `/anime/${anime.mal_id}`,
+                    alt: anime.title,
+                })) ?? []}
+            />
+            <HorizontalCarousel
+                heading={Vernac.getVernac('HP_TOP_MANGA_TITLE')}
+                useQueryHook={useGetTopMangaQuery}
+                options={{}}
+                adapter={(data) => data?.data.map((manga) => ({
+                    title: manga.title,
+                    imageUrl: manga.images.jpg.image_url,
+                    navigateTo: `/manga/${manga.mal_id}`,
+                    alt: manga.title,
+                })) ?? []}
+            />
             <LazyMount estimatedHeight={300}>
                 <HorizontalCarousel
-                    useQueryHook={useGetTopAnimeQuery}
+                    heading={Vernac.getVernac('HP_TOP_CHARACTER_TITLE')}
+                    useQueryHook={useGetTopCharactersQuery}
                     options={{}}
-                    adapter={(data) => data?.data.map((anime) => ({
-                        title: anime.title,
-                        imageUrl: anime.images.jpg.image_url,
-                        navigateTo: `/anime/${anime.mal_id}`,
-                        alt: anime.title,
+                    adapter={(data) => data?.data.map((character) => ({
+                        title: character.name,
+                        imageUrl: character.images.jpg.image_url,
+                        navigateTo: `/character/${character.mal_id}`,
+                        alt: character.name,
                     })) ?? []}
                 />
             </LazyMount>
             <LazyMount estimatedHeight={300}>
                 <HorizontalCarousel
-                    useQueryHook={useGetTopMangaQuery}
+                    heading={Vernac.getVernac('HP_TOP_PEOPLE_TITLE')}
+                    useQueryHook={useGetTopPeopleQuery}
                     options={{}}
-                    adapter={(data) => data?.data.map((anime) => ({
-                        title: anime.title,
-                        imageUrl: anime.images.jpg.image_url,
-                        navigateTo: `/anime/${anime.mal_id}`,
-                        alt: anime.title,
-                    })) ?? []}
-                />
-            </LazyMount>
-            <LazyMount estimatedHeight={300}>
-                <HorizontalCarousel
-                    useQueryHook={useGetAnimeSeasonsNowQuery}
-                    options={{}}
-                    adapter={(data) => data?.data.map((anime) => ({
-                        title: anime.title,
-                        imageUrl: anime.images.jpg.image_url,
-                        navigateTo: `/anime/${anime.mal_id}`,
-                        alt: anime.title,
-                    })) ?? []}
-                />
-            </LazyMount>
-            <LazyMount estimatedHeight={300}>
-                <HorizontalCarousel
-                    useQueryHook={useGetAnimeSeasonsUpcomingQuery}
-                    options={{}}
-                    adapter={(data) => data?.data.map((anime) => ({
-                        title: anime.title,
-                        imageUrl: anime.images.jpg.image_url,
-                        navigateTo: `/anime/${anime.mal_id}`,
-                        alt: anime.title,
+                    adapter={(data) => data?.data.map((person) => ({
+                        title: person.name,
+                        imageUrl: person.images.jpg.image_url,
+                        navigateTo: `/person/${person.mal_id}`,
+                        alt: person.name,
                     })) ?? []}
                 />
             </LazyMount>
