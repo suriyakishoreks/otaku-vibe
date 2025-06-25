6ea67544df2ed241ce73cf1ce1fa34b982a31348
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-06-25 19:55:07 +0530

    feat: add landing page vernac strings

--- src/services/vernac/en.ts
+++ src/services/vernac/en.ts
@@ -5,7 +5,20 @@ const enVernacConfig: VernacConfig = {
     HOME: 'Home',
     ANIME: 'Anime',
     MANGA: 'Manga',
-    COPY_RIGHT: 'Otaku Vibe. All rights reserved.'
+    COPY_RIGHT: 'Otaku Vibe. All rights reserved.',
+    HP_TOP_ANIME_TITLE: "Fandom's Favorite Anime",
+    HP_TOP_MANGA_TITLE: "Read What Everyone's Loving.",
+    HP_TOP_CHARACTER_TITLE: "Top Characters: Who Will You Root For?",
+    HP_TOP_PEOPLE_TITLE: "Behind the Scenes: Top Industry Figures",
+    ALP_CURRENT_SEASON_TITLE: 'Uncover Your Next Anime Obsession',
+    ALP_UPCOMING_SEASON_TITLE: 'Get Ready: Upcoming Anime Season.',
+    ALP_TOP_RATED_TITLE: 'Must-Watch Anime Classics.',
+    ALP_RECOMMENDATION_TITLE: 'Just For You: Anime Recommendations.',
+    ALP_SCHEDULE_TITLE: 'Your Weekly Anime Lineup',
+    MLP_TRENDING_MANGA_TITLE: 'Trending Manga: What\'s Hot Now',
+    MLP_TRENDING_MANHWA_TITLE: 'The Hottest Manhwa Series.',
+    MLP_TRENDING_MANHUA_TITLE: 'Discover Popular Manhua',
+    MLP_RECOMMENDATION_TITLE: 'Just For You: Manga Recommendations.'
 };
 
 export default enVernacConfig;
--- src/services/vernac/models/vernac.model.ts
+++ src/services/vernac/models/vernac.model.ts
@@ -4,6 +4,20 @@ export interface VernacConfig {
     ANIME: string;
     MANGA: string;
     COPY_RIGHT: string;
+    HP_TOP_ANIME_TITLE: string;
+    HP_TOP_MANGA_TITLE: string;
+    HP_TOP_CHARACTER_TITLE: string;
+    HP_TOP_PEOPLE_TITLE: string;
+    ALP_CURRENT_SEASON_TITLE: string;
+    ALP_UPCOMING_SEASON_TITLE: string;
+    ALP_TOP_RATED_TITLE: string;
+    ALP_RECOMMENDATION_TITLE: string;
+    ALP_SCHEDULE_TITLE: string;
+    MLP_TRENDING_MANGA_TITLE: string;
+    MLP_TRENDING_MANHWA_TITLE: string;
+    MLP_TRENDING_MANHUA_TITLE: string;
+    MLP_RECOMMENDATION_TITLE: string;
+
 }
 
-export type Locale = 'en';
+export type Locale = 'en' | 'jp';
