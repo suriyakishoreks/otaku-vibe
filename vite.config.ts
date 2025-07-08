e1044a4ae8f6ff7aac31ed8e56637f3ac733f30f
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-07-08 20:45:03 +0530

    refactor: manifest properties

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import stylelint from 'vite-plugin-stylelint';
 import eslint from 'vite-plugin-eslint';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const BASE_PATH = '/otaku-vibe/';
+
 export default defineConfig({
-  base: '/otaku-vibe/',
+  base: BASE_PATH,
   plugins: [
     react({
       babel: {
@@ -18,42 +20,44 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: false
       },
       manifest: {
-        "name": "OtakuVibe",
-        "short_name": "OtakuVibe",
-        "icons": [
+        name: "OtakuVibe",
+        short_name: "OtakuVibe",
+        id: BASE_PATH,
+        scope: BASE_PATH,
+        start_url: BASE_PATH,
+        icons: [
           {
-            "src": "/pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            src: BASE_PATH + "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
           },
           {
-            "src": "/pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
+            src: BASE_PATH + "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
           },
           {
-            "src": "/pwa-maskable-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: BASE_PATH + "pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
           },
           {
-            "src": "/pwa-maskable-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: BASE_PATH + "pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
           }
         ],
-        "start_url": "/",
-        "display": "standalone",
-        "background_color": "#000000",
-        "theme_color": "#FFFFFF",
-        "description": "A unified platform to explore the world of anime, manga, manhwa and manhua."
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#FFFFFF",
+        description: "A unified platform to explore the world of anime, manga, manhwa and manhua."
       }
     }),
     stylelint({
