956d6ade6d5103edbdf01783272a3af30c3d6f44
Suriyakishore K S <kssuriyakishore16@gmail.com>
2025-07-08 20:22:28 +0530

    chore: pwa integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import stylelint from 'vite-plugin-stylelint';
 // @ts-expect-error: vite-plugin-eslint may not have TypeScript types
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/otaku-vibe/',
@@ -13,6 +14,47 @@ export default defineConfig({
           ['babel-plugin-react-compiler'],
         ],
       },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        "name": "OtakuVibe",
+        "short_name": "OtakuVibe",
+        "icons": [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": "/",
+        "display": "standalone",
+        "background_color": "#000000",
+        "theme_color": "#FFFFFF",
+        "description": "A unified platform to explore the world of anime, manga, manhwa and manhua."
+      }
     }),
     stylelint({
       files: ['**/*.{css,scss,html}'],
